7316d27cca6790306a9ddc766b0b8f6d2e6dda08
v.moving.momentum <v.moving.momentum@gmail.com>
2025-06-01 13:37:03 +0800

    initial commit

---
+++ site.config.js
@@ -0,0 +1,11 @@
+export const CONTACT = {
+  EMAIL: 'v.moving.momentum+hoursinfo@gmail.com',
+  INSTAGRAM: '@vmovingmomentumapps',
+  APP_NAME: 'Hours',
+  COMPANY_NAME: 'V Moving Momentum',
+  WEBSITE: 'https://v-moving-momentum.github.io',
+  GOOGLE_PLAY_URL: 'https://play.google.com/store/apps/details?id=com.example.hours', // TODO: Replace with actual link
+  INSTAGRAM_URL: 'https://instagram.com/vmovingmomentumapps',
+  TWITTER_URL: '', // Add if available
+  FACEBOOK_URL: '' // Add if available
+}; 
